6da99b74c8d4bcc072ea8bd6f9787724d95372d6
Dominic <dominicnunez@gmail.com>
2025-08-12 21:56:59 -0500

    Setup VS coding settings file

--- modules/home-manager/applications.nix
+++ modules/home-manager/applications.nix
@@ -67,58 +67,277 @@
       default = {
         # User settings
         userSettings = {
-      "editor.fontSize" = 13;
-      "editor.fontFamily" = "'JetBrains Mono', 'Droid Sans Mono', monospace";
-      "editor.fontLigatures" = true;
-      "editor.lineHeight" = 22;
-      "editor.rulers" = [80 120];
-      "editor.wordWrap" = "on";
-      "editor.minimap.enabled" = false;
-      "editor.renderWhitespace" = "selection";
-      "editor.suggestSelection" = "first";
-      "editor.acceptSuggestionOnCommitCharacter" = false;
+          # Editor settings
+          "editor.fontSize" = 14;
+          "editor.fontFamily" = "'JetBrainsMono Nerd Font', 'JetBrains Mono', 'Cascadia Code', 'Fira Code', monospace";
+          "editor.fontLigatures" = true;
+          "editor.lineHeight" = 22;
+          "editor.rulers" = [80 120];
+          "editor.wordWrap" = "on";
+          "editor.minimap.enabled" = false;
+          "editor.renderWhitespace" = "selection";
+          "editor.suggestSelection" = "first";
+          "editor.acceptSuggestionOnCommitCharacter" = false;
+          "editor.formatOnSave" = true;
+          "editor.formatOnPaste" = true;
+          "editor.inlineSuggest.enabled" = true;
+          "editor.cursorStyle" = "line";
+          "editor.smoothScrolling" = true;
+          "editor.cursorBlinking" = "smooth";
+          "editor.bracketPairColorization.enabled" = true;
+          "editor.guides.bracketPairs" = "active";
+          "editor.stickyScroll.enabled" = true;
+          "editor.linkedEditing" = true;
+          "editor.tabSize" = 2;
 
-      "workbench.colorTheme" = "Dracula";
-      "workbench.iconTheme" = "material-icon-theme";
-      "workbench.startupEditor" = "none";
+          # Workbench settings
+          "workbench.colorTheme" = "Dracula";
+          "workbench.iconTheme" = "material-icon-theme";
+          "workbench.startupEditor" = "none";
+          "workbench.sideBar.location" = "left";
+          "workbench.activityBar.location" = "default";
+          "workbench.statusBar.visible" = true;
+          "workbench.tree.indent" = 15;
+          "workbench.tree.renderIndentGuides" = "always";
+          "workbench.editor.enablePreview" = false;
+          "workbench.editor.revealIfOpen" = true;
 
-      "terminal.integrated.fontSize" = 13;
-      "terminal.integrated.fontFamily" = "'JetBrains Mono'";
-      "terminal.integrated.defaultProfile.linux" = "bash";
+          # Terminal settings
+          "terminal.integrated.fontSize" = 14;
+          "terminal.integrated.fontFamily" = "'JetBrainsMono Nerd Font', 'JetBrains Mono'";
+          "terminal.integrated.defaultProfile.linux" = "bash";
+          "terminal.integrated.cursorBlinking" = true;
+          "terminal.integrated.cursorStyle" = "line";
+          "terminal.integrated.smoothScrolling" = true;
+          "terminal.integrated.copyOnSelection" = true;
 
-      "files.autoSave" = "afterDelay";
-      "files.autoSaveDelay" = 1000;
-      "files.trimTrailingWhitespace" = true;
-      "files.insertFinalNewline" = true;
+          # File settings
+          "files.autoSave" = "afterDelay";
+          "files.autoSaveDelay" = 1000;
+          "files.trimTrailingWhitespace" = true;
+          "files.insertFinalNewline" = true;
+          "files.trimFinalNewlines" = true;
+          "files.eol" = "\n";
+          "files.exclude" = {
+            "**/.git" = false;
+            "**/.DS_Store" = true;
+            "**/node_modules" = true;
+            "**/__pycache__" = true;
+            "**/*.pyc" = true;
+          };
+          "files.watcherExclude" = {
+            "**/.git/objects/**" = true;
+            "**/.git/subtree-cache/**" = true;
+            "**/node_modules/**" = true;
+            "**/.venv/**" = true;
+            "**/venv/**" = true;
+          };
 
-      "git.enableSmartCommit" = true;
-      "git.confirmSync" = false;
-      "git.autofetch" = true;
+          # Explorer settings
+          "explorer.confirmDelete" = false;
+          "explorer.confirmDragAndDrop" = false;
+          "explorer.compactFolders" = false;
 
-      "extensions.ignoreRecommendations" = false;
-      "telemetry.telemetryLevel" = "off";
+          # Git settings
+          "git.enableSmartCommit" = true;
+          "git.confirmSync" = false;
+          "git.autofetch" = true;
+          "git.autoStash" = true;
+          "git.openRepositoryInParentFolders" = "always";
 
-      # Language-specific settings
-      "[python]" = {
-        "editor.formatOnSave" = true;
-        "editor.defaultFormatter" = "ms-python.black-formatter";
-      };
-      "[javascript]" = {
-        "editor.formatOnSave" = true;
-        "editor.defaultFormatter" = "esbenp.prettier-vscode";
-      };
-      "[typescript]" = {
-        "editor.formatOnSave" = true;
-        "editor.defaultFormatter" = "esbenp.prettier-vscode";
-      };
-      "[rust]" = {
-        "editor.formatOnSave" = true;
-      };
-      "[nix]" = {
-        "editor.formatOnSave" = true;
-        "editor.defaultFormatter" = "jnoortheen.nix-ide";
-      };
-    };
+          # Search settings
+          "search.exclude" = {
+            "**/node_modules" = true;
+            "**/bower_components" = true;
+            "**/*.code-search" = true;
+            "**/.venv" = true;
+            "**/venv" = true;
+            "**/__pycache__" = true;
+          };
+          "search.useIgnoreFiles" = true;
+          "search.followSymlinks" = false;
+
+          # Extension settings
+          "extensions.ignoreRecommendations" = false;
+          "extensions.autoUpdate" = true;
+          "extensions.autoCheckUpdates" = true;
+
+          # Telemetry
+          "telemetry.telemetryLevel" = "off";
+
+          # Window settings
+          "window.zoomLevel" = 0;
+          "window.menuBarVisibility" = "toggle";
+
+          # Update settings
+          "update.mode" = "manual";
+          "update.showReleaseNotes" = false;
+
+          # Security
+          "security.workspace.trust.untrustedFiles" = "open";
+
+          # Language-specific formatters and settings
+          "[javascript]" = {
+            "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            "editor.formatOnSave" = true;
+            "editor.tabSize" = 2;
+          };
+          "[javascriptreact]" = {
+            "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            "editor.formatOnSave" = true;
+            "editor.tabSize" = 2;
+          };
+          "[typescript]" = {
+            "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            "editor.formatOnSave" = true;
+            "editor.tabSize" = 2;
+          };
+          "[typescriptreact]" = {
+            "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            "editor.formatOnSave" = true;
+            "editor.tabSize" = 2;
+          };
+          "[json]" = {
+            "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            "editor.formatOnSave" = true;
+            "editor.tabSize" = 2;
+          };
+          "[jsonc]" = {
+            "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            "editor.formatOnSave" = true;
+            "editor.tabSize" = 2;
+          };
+          "[html]" = {
+            "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            "editor.formatOnSave" = true;
+            "editor.tabSize" = 2;
+          };
+          "[css]" = {
+            "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            "editor.formatOnSave" = true;
+            "editor.tabSize" = 2;
+          };
+          "[scss]" = {
+            "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            "editor.formatOnSave" = true;
+            "editor.tabSize" = 2;
+          };
+          "[markdown]" = {
+            "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            "editor.formatOnSave" = true;
+            "editor.wordWrap" = "on";
+          };
+          "[yaml]" = {
+            "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            "editor.formatOnSave" = true;
+            "editor.tabSize" = 2;
+          };
+          "[nix]" = {
+            "editor.defaultFormatter" = "jnoortheen.nix-ide";
+            "editor.formatOnSave" = true;
+            "editor.tabSize" = 2;
+          };
+          "[python]" = {
+            "editor.defaultFormatter" = "ms-python.black-formatter";
+            "editor.formatOnSave" = true;
+            "editor.tabSize" = 4;
+          };
+          "[rust]" = {
+            "editor.defaultFormatter" = "rust-lang.rust-analyzer";
+            "editor.formatOnSave" = true;
+            "editor.tabSize" = 4;
+          };
+          "[go]" = {
+            "editor.defaultFormatter" = "golang.go";
+            "editor.formatOnSave" = true;
+            "editor.tabSize" = 4;
+          };
+          "[dockerfile]" = {
+            "editor.defaultFormatter" = "ms-azuretools.vscode-docker";
+            "editor.formatOnSave" = true;
+          };
+          "[toml]" = {
+            "editor.defaultFormatter" = "tamasfe.even-better-toml";
+            "editor.formatOnSave" = true;
+          };
+
+          # Prettier configuration
+          "prettier.semi" = true;
+          "prettier.singleQuote" = true;
+          "prettier.tabWidth" = 2;
+          "prettier.useTabs" = false;
+          "prettier.trailingComma" = "es5";
+          "prettier.bracketSpacing" = true;
+          "prettier.arrowParens" = "avoid";
+          "prettier.endOfLine" = "lf";
+
+          # Python settings
+          "python.defaultInterpreterPath" = "python3";
+          "python.linting.enabled" = true;
+          "python.linting.lintOnSave" = true;
+          "python.formatting.provider" = "black";
+          "python.linting.pylintEnabled" = false;
+          "python.linting.flake8Enabled" = true;
+          "python.testing.pytestEnabled" = true;
+          "python.testing.unittestEnabled" = false;
+
+          # JavaScript/TypeScript settings
+          "javascript.updateImportsOnFileMove.enabled" = "always";
+          "typescript.updateImportsOnFileMove.enabled" = "always";
+          "javascript.suggest.autoImports" = true;
+          "typescript.suggest.autoImports" = true;
+
+          # ESLint settings
+          "eslint.validate" = [
+            "javascript"
+            "javascriptreact"
+            "typescript"
+            "typescriptreact"
+          ];
+          "eslint.autoFixOnSave" = true;
+
+          # Emmet settings
+          "emmet.includeLanguages" = {
+            "javascript" = "javascriptreact";
+            "typescript" = "typescriptreact";
+          };
+          "emmet.triggerExpansionOnTab" = true;
+
+          # Bracket pair colorization
+          "editor.bracketPairColorization.independentColorPoolPerBracketType" = true;
+
+          # GitLens settings
+          "gitlens.currentLine.enabled" = false;
+          "gitlens.codeLens.enabled" = false;
+          "gitlens.hovers.currentLine.over" = "line";
+
+          # Error Lens settings (when installed)
+          "errorLens.enabled" = true;
+          "errorLens.enabledDiagnosticLevels" = ["error" "warning" "info"];
+
+          # Live Server settings (when installed)
+          "liveServer.settings.donotShowInfoMsg" = true;
+          "liveServer.settings.donotVerifyTags" = true;
+
+          # Material Icon Theme settings
+          "material-icon-theme.folders.theme" = "specific";
+          "material-icon-theme.activeIconPack" = "nest";
+
+          # Todo Tree settings (when installed)
+          "todo-tree.highlights.enabled" = true;
+          "todo-tree.highlights.defaultHighlight" = {
+            "icon" = "alert";
+            "type" = "text";
+            "foreground" = "#ff0000";
+            "background" = "#ffffff";
+            "opacity" = 50;
+            "iconColour" = "#0000ff";
+          };
+
+          # Spell Checker settings (when installed)
+          "cSpell.language" = "en";
+          "cSpell.userWords" = [];
+        };
 
         # Extensions to install
         extensions = with pkgs.vscode-extensions; [
@@ -153,6 +372,7 @@
       # Rust
       rust-lang.rust-analyzer
       tamasfe.even-better-toml
+      # dependi (need to get actual ext name)
 
       # Go
       golang.go
